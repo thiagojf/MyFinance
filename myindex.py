3e46b619b6c1837091bcbb8b20ed52afaf8742ef
Thiago Faria <thiago_faria@outlook.com>
2024-01-24 14:56:07 -0300

    codigos iniciais para criar a divisão da sideber com a dash

--- myindex.py
+++ myindex.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
-
+from components import sidebar, dashboards, extratos
 
 
 
@@ -15,15 +15,27 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
-
-
-
-
+    dbc.Row(
+        dbc.col([
+         dcc.Location(id='url'),
+         sidebar.layout   
+        ], md=2, style={'background-color': 'red', 'heigt': '1080px'}),
+        
+        dbc.col([
+            content
+        ], md=10, style={'background-color': 'blue', 'heigt': '1080px'})
+    )
 
 ], fluid=True,)
 
 
-
+@app.collback(Output('page-content', 'children'),[Input('url', 'pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname == '/dashboards':
+        return dashboards.layout
+    
+    if pathname == '/extratos' :
+        return extratos.layout
 
 if __name__ == '__main__':
     app.run_server(port=8051, debug=True)
